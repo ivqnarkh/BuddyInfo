f78518c7ec204e502d9483d2799b7ced7791074a
= <=>
2025-09-15 13:32:12 -0400

    created buddy in main

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -16,6 +16,9 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Chungus Avenue", "1234512");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
